230899afec36f45705a7ccd650eb9f3aebb7af56
Daniel Cho <thedanielcho@gmail.com>
2021-05-17 09:02:25 -0400

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-users = User.create([
-  {email: "thedanielcho@gmail.com", display_name: "Daniel Cho", password: "password"},
-  {email: "demo@mail.com", display_name: "demo user", password: "password"}
-])
 
-channels = Channel.create([
-  {name: "general", description: "General chat for everyone", public?: true, admin_id: 1},
-  {name: "test1", public?: true, admin_id: 1},
-  {name: "test2", public?: true, admin_id: 1},
-])
+User.destroy_all
 
-memberships = Membership.create([
-  {user_id: 1, memberable_type: "Channel", memberable_id: 1}
-  {user_id: 2, memberable_type: "Channel", memberable_id: 1}
-])
+Channel.destroy_all
+
+Membership.destroy_all
+
+user1 = User.create!(email: "thedanielcho@gmail.com", display_name: "Daniel Cho", password: "password")
+user2 = User.create!(email: "demo@mail.com", display_name: "demo user", password: "password")
+
+channel1 = Channel.create!(name: "general", description: "General chat for everyone", public?: true, admin_id: 1)
+channel2 = Channel.create!(name: "test1", public?: true, admin_id: 1)
+channel3 = Channel.create!(name: "test2", public?: true, admin_id: 1)
+
+membership1 = Membership.create!(user_id: 1, memberable_type: "Channel", memberable_id: 1)
+membership2 = Membership.create!(user_id: 2, memberable_type: "Channel", memberable_id: 1)
+
 
