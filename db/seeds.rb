a3ee203f211fba5b396b9a68fcadb925ff17f6cc
Daniel Cho <thedanielcho@gmail.com>
2021-05-17 09:06:07 -0400

    fixed seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,12 @@ Membership.destroy_all
 user1 = User.create!(email: "thedanielcho@gmail.com", display_name: "Daniel Cho", password: "password")
 user2 = User.create!(email: "demo@mail.com", display_name: "demo user", password: "password")
 
-channel1 = Channel.create!(name: "general", description: "General chat for everyone", public?: true, admin_id: 1)
-channel2 = Channel.create!(name: "test1", public?: true, admin_id: 1)
-channel3 = Channel.create!(name: "test2", public?: true, admin_id: 1)
+channel1 = Channel.create!(name: "general", description: "General chat for everyone", public?: true, admin_id: user1.id)
+channel2 = Channel.create!(name: "test1", public?: true, admin_id: user1.id)
+channel3 = Channel.create!(name: "test2", public?: true, admin_id: user1.id)
 
-membership1 = Membership.create!(user_id: 1, memberable_type: "Channel", memberable_id: 1)
-membership2 = Membership.create!(user_id: 2, memberable_type: "Channel", memberable_id: 1)
-
-
+membership1 = Membership.create!(user_id: user1.id, memberable_type: "Channel", memberable_id: channel1)
+membership2 = Membership.create!(user_id: user2.id, memberable_type: "Channel", memberable_id: channel1)
+membership3 = Membership.create!(user_id: user1.id, memberable_type: "Channel", memberable_id: channel2)
+membership4 = Membership.create!(user_id: user2.id, memberable_type: "Channel", memberable_id: channel2)
+membership5 = Membership.create!(user_id: user1.id, memberable_type: "Channel", memberable_id: channel3)
